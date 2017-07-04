8e06893c21ab25983593971c83de09f16243d56d
Ticat Wolves <ticatwolves@gmail.com>
2017-07-04 18:40:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='JHoroscope',
+    version='1.0.0',
+    description='Fetches and parses data from theastrologer.',
+    author='Ticat Wolves',
+    author_email='ticatwolves@gmail.com',
+    url='https://github.com/ticatwolves/JHoroscope',
+    packages=['JHoroscope'],
+)
